490f9c5458c78a918dc7d50ddee64c41adf86079
Baniya-sen <144620117+Baniya-sen@users.noreply.github.com>
2024-06-05 13:39:54 +0530

    adding files

---
+++ assets/js/script.js
@@ -0,0 +1,79 @@
+const dots = document.querySelectorAll('.dot');
+const sections = document.querySelectorAll('.section');
+
+dots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        if (!dot.classList.contains('active')) {
+            dots.forEach(dot => dot.classList.remove('active'));
+            dot.classList.add('active');
+
+            const currentSection = document.querySelector('.section.active');
+            currentSection.classList.add('fade-out');
+            setTimeout(() => {currentSection.classList.remove('active')}, 350);
+
+            sections[index].classList.add('fade-in');
+            setTimeout(() => {
+                sections.forEach(section => section.classList.remove('fade-out'));
+                sections[index].classList.add('active');
+            }, 400);
+
+            const navOptions = document.querySelectorAll('.option');
+
+            navOptions.forEach((op) => {
+              const text1 = op.querySelector('p').textContent;
+              op.querySelector('p').textContent = text1;
+            });
+
+            const text = navOptions[index].querySelector('p').textContent;
+            navOptions[index].querySelector('p').innerHTML = `<s>${text}</s>`;
+        }
+    });
+});
+
+
+document.querySelector('.nav-toggle').addEventListener('click', () => {
+  document.querySelector('.container').classList.add('nav');
+  document.querySelector('.nav-toggle').classList.add('active');
+  document.querySelector('.nav-options').classList.add('active');
+  document.querySelector('.dabba').classList.add('off');
+
+  document.querySelector('.container').addEventListener('click', () => {
+    document.querySelector('.container').classList.remove('nav');
+    document.querySelector('.nav-toggle').classList.remove('active');
+    document.querySelector('.nav-options').classList.remove('active');
+    document.querySelector('.dabba').classList.remove('off');
+  });
+});
+
+
+const navOptions = document.querySelectorAll('.option');
+
+navOptions.forEach((option, index) => {
+  option.addEventListener('click', () => {
+
+    navOptions.forEach((op) => {
+      const text1 = op.querySelector('p').textContent;
+      op.querySelector('p').innerHTML = text1;
+    });
+
+    const text = option.querySelector('p').textContent;
+    option.querySelector('p').innerHTML = `<s>${text}</s>`;
+
+    document.querySelector('.container').classList.remove('nav');
+    document.querySelector('.nav-toggle').classList.remove('active');
+    document.querySelector('.nav-options').classList.remove('active');
+    document.querySelector('.dabba').classList.remove('off');
+
+    dots.forEach(dot => dot.classList.remove('active'));
+    dots[index].classList.add('active');
+
+    const currentSection = document.querySelector('.section.active');
+            currentSection.classList.add('fade-out');
+            setTimeout(() => {currentSection.classList.remove('active')}, 350);
+            sections[index].classList.add('fade-in');
+            setTimeout(() => {
+                sections.forEach(section => section.classList.remove('fade-out'));
+                sections[index].classList.add('active');
+            }, 400);
+  });
+});
